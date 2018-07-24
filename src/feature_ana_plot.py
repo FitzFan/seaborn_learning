86c3db8ea586f192cfc5ef72b27157bcb2578dd3
Ryan Fan <ryanfan0528@gmail.com>
2018-07-24 21:54:37 +0800

    keep learning

--- src/feature_ana_plot.py
+++ src/feature_ana_plot.py
@@ -11,13 +11,15 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy import stats
+from scipy.stats import norm
 import matplotlib.pyplot as plt
+
 
 """
 - Show the counts of observations in each categorical bin using bars.
 - 单离散型变量的取值分布图，可以加上分类变量
 """
-def cate_sig_dis(data_frame, col_name, hue_name=None):
+def cate_sig_dis(data_frame, col_name, hue_val=None):
 	# 将画布设置为A4纸
 	a4_dims = (11.7, 8.27)
 
@@ -26,12 +28,12 @@ def cate_sig_dis(data_frame, col_name, hue_name=None):
 		f, ax = plt.subplots(figsize=a4_dims)
 
 		# 判断绘图类型
-		if hue_name is None:
+		if hue_val is None:
 			# 绘图
-			fig = sns.countplot(x=col_name,  data=data_frame, palette="Set3")
+			fig = sns.countplot(x=col_name, data=data_frame, palette="Set3")
 		else:
 			# 绘图
-			fig = sns.countplot(x=col_name, hue=hue_name, data=data_frame, palette="Set3")
+			fig = sns.countplot(x=col_name, hue=hue_val, data=data_frame, palette="Set3")
 
 		# 设置标题
 		# fig.set_title('Value Distribution Of %s'%(col_name), weight='bold')
@@ -52,9 +54,10 @@ def cont_sig_dis(data_frame, col_name):
 		f, ax = plt.subplots(figsize=a4_dims)
 
 		# 绘图
-		fig = sns.distplot(a=data_frame[data_frame[col_name].notnull()][col_name], hist=True, rug=True)
+		fig = sns.distplot(a=data_frame[data_frame[col_name].notnull()][col_name])
 
 	plt.show()
+
 
 """
 - Fit and plot a univariate or bivariate kernel density estimate.
@@ -81,7 +84,7 @@ def cont_biv_dis(data_frame, x_val_1, y_val_1, x_val_2=None, y_val_2=None):
 	- y轴的变量类型必须是numeric;
 	- 传入的estimator一般是来自numpy package, 比如np.mean, np.max, np.median
 """
-def cate_biv_dis(data_frame, x_val, y_val, hue_name=None, estimator=None):
+def cate_biv_dis(data_frame, x_val, y_val, hue_val=None, estimator=None):
 	# 将画布设置为A4纸
 	a4_dims = (11.7, 8.27)
 
@@ -90,7 +93,7 @@ def cate_biv_dis(data_frame, x_val, y_val, hue_name=None, estimator=None):
 		f, ax = plt.subplots(figsize=a4_dims)
 
 		# 判断绘图类型
-		if hue_name is None:
+		if hue_val is None:
 			# 绘图
 			if estimator is None:
 				fig = sns.barplot(x=x_val, y=y_val, data=data_frame, palette="Set3")
@@ -99,9 +102,9 @@ def cate_biv_dis(data_frame, x_val, y_val, hue_name=None, estimator=None):
 		else:
 			# 绘图
 			if estimator is None:
-				fig = sns.barplot(x=x_val, y=y_val, data=data_frame, hue=hue_name, palette="Set3")
+				fig = sns.barplot(x=x_val, y=y_val, data=data_frame, hue=hue_val, palette="Set3")
 			else:
-				fig = sns.barplot(x=x_val, y=y_val, data=data_frame, hue=hue_name, estimator=estimator, palette="Set3")
+				fig = sns.barplot(x=x_val, y=y_val, data=data_frame, hue=hue_val, estimator=estimator, palette="Set3")
 
 		# 设置标题
 		# fig.set_title('Bar Plot Of %s and %s'%(x_val, y_val), weight='bold')
@@ -162,4 +165,152 @@ def cont_biv_rel(data_frame, val_1, val_2, stat_func=stats.pearsonr, kind="reg")
 		ax = sns.jointplot(val_1, val_2, data=data_frame, kind=kind, stat_func=stat_func)
 
 	plt.show()
+
+
+"""
+- 对角线上的图，就是单一连续变量的分布图，纵轴是坐标是迷惑。
+- 只适合continuous变量间的对比。
+- 可传入一个离散型变量作为hue_val。
+- 特殊参数解析：【使用频率不高】
+	- diag_kind：对角线的图形设定，默认是直方图。可以设为“kde”改为核密度图
+	- kind：给非单变量图增加画图样式
+"""
+def cont_pair_analyse(data_frame, hue_val=None):
+	with sns.axes_style("darkgrid"): 
+		if hue_val is None:
+			fig = sns.pairplot(data_frame, palette="Set3")
+		else:
+			fig = sns.pairplot(data_frame, hue=hue_val, palette="Set3")
+
+	plt.show()
+
+
+"""
+- 多变量散点图
+"""
+def cont_biv_strip(data_frame, x_val, y_val):
+	with sns.axes_style("darkgrid"): 
+		f, ax = plt.subplots(figsize=a4_dims)
+		fig = sns.regplot(x=x_val, y=y_val, data=data_frame, fit_reg=True)
+
+	plt.show()
+
+
+"""
+- 箱线图
+- 一般情况：
+	- 横轴是离散型变量
+	- 纵轴是连续型变量
+- 当然横轴也可以是连续型变量，但或比较难看~
+"""
+def box_plot(data_frame, x_val, y_val):
+	with sns.axes_style("darkgrid"):
+		f, ax = plt.subplots(figsize=a4_dims)
+		fig = sns.boxplot(x=x_val, y=y_val, data=data_frame, ax=ax, palette="Set3")
+
+	plt.show()
+
+"""
+- 相关系数矩阵，用热力图呈现
+- 热力图参数说明：
+	- annot默认为False，当annot为True时，在heatmap中每个方格写入数据，annot_kws，当annot为True时，可设置各个参数，包括大小，颜色，加粗，斜体字等
+	- fmt: String formatting code to use when adding annotations.
+	- linewidths: Width of the lines that will divide each cell.
+	- cbar: Whether to draw a colorbar.
+	- square: If True, set the Axes aspect to “equal” so each cell will be square-shaped.
+	- annot_kws: Keyword arguments for ax.text when annot is True.
+	- linewidths: Width of the lines that will divide each cell. 相邻单元格之间的距离
+"""
+def corr_heat_map(data_frame, cor_thre=0.6, top_num=None, tar_val=None):
+	# 计算相关系数矩阵
+	corrmat = data_frame.corr()
+
+	# 热力图是全部展示，还是只展示跟某个变量相关性比较强的变量
+	if top_num not None and tar_val is not None:
+		cols_max = corrmat.nlargest(top_num, tar_val)[tar_val].index   # 取出与saleprice相关性最大的十项
+		cols_min = corrmat.nsmallest(top_num, tar_val)[tar_val].index  # 取出与saleprice相关性最小的十项
+		cols = list(cols_max) + list(cols_min)
+		cols = list(set(cols))
+		cm = np.corrcoef(data_frame[cols].values.T)  #相关系数
+	else:
+		cm = corrmat
+
+	# 绘制热力图
+	with sns.axes_style("darkgrid"):
+		f, ax = plt.subplots(figsize=a4_dims) 
+		hm_first = sns.heatmap(cm, linewidths=0.1, cbar=True, vmin=-1, vmax=1, center=0,
+							  annot=True, square=True, fmt='.2f', annot_kws={'size': 8}, 
+							  yticklabels=cols, xticklabels=cols, cmap='RdBu')
+
+	# 只突出一些特殊的数字
+	for text in hm_first.texts:
+		value_num = abs(float(text.get_text())) # 取绝对值
+		if value_num >= cor_thre and value_num != 1.00:
+			text.set_fontsize(8)
+			text.set_weight('bold')
+			text.set_color('orangered')
+		else:
+			text.set_alpha(0)
+
+	# 设置标题
+	if tar_val is not None:
+		hm_first.set_title('Plot rectangular data as a color-encoded matrix of important variables:'%tar_val, weight='bold')
+	else:
+		hm_first.set_title('Plot rectangular data as a color-encoded matrix', weight='bold')
+
+	plt.show()
+
+
+"""
+- 正态性检验
+	- 条形图+正态曲线拟合
+	- Q-Q图
+	- 传入的变量格式类似：df_train['SalePrice']
+"""
+def cont_nor_dis(Series):
+	# 将画布设置为A4纸
+	a4_dims = (11.7, 8.27)
+
+	# 画布1
+	with sns.axes_style("darkgrid"):
+		# 设置画布大小
+		f, ax= plt.subplots(figsize=a4_dims)
+
+		# 绘制正态曲线拟合
+		fig = sns.distplot(Series, fit=norm, ax=ax) # fit 控制拟合的参数分布图形
+		fig.set_title('Flexibly plot a univariate distribution of observations.', weight='bold') # 设置标题
+
+	# 画布2
+	with sns.axes_style("darkgrid"):
+		# 设置画布大小
+		f, ax= plt.subplots(figsize=a4_dims)
+
+		# probplot :Calculate quantiles for a probability plot, and optionally show the plot. 计算概率图的分位数
+		res = stats.probplot(Series, plot=plt) # seaborn画不了Q-Q图，这里使用states.probplot()来完成Q-Q图
+
+	plt.show()
+
+	# 如果遇到不符合正态分布的feature，可以使用log()转换
+	# train_df['SalePrice'] = np.log(train_df['SalePrice'])
+
+
+"""
+批量画连续型变量的分布
+"""
+def multi_cont_dis(data_frame, cont_val_list, facet_num=4):
+	#  检查传入的变量是否符合要求
+	for col_name in cont_val_list:
+		if data_frame.dtypes[col_name] == 'object':
+			print 'Please Input Quantitative feature'
+			sys.exit()
+
+	with sns.axes_style("darkgrid"):
+		f = pd.melt(data_frame, value_vars=cont_val_list)
+		g = sns.FacetGrid(f, col="variable",  col_wrap=facet_num, sharex=False, sharey=False)
+		g = g.map(sns.distplot, "value")
+		
+	plt.show()
+		
+
+
 
--- src/seaborn_learning.py
+++ src/seaborn_learning.py
@@ -92,78 +92,105 @@ a4_dims = (11.7, 8.27) # A4纸大小
 # sys.exit()
 
 # 双变量关系图
-tips = sns.load_dataset("tips")
-with sns.axes_style("darkgrid"): 
-    ax = sns.jointplot("total_bill", "tip", data=tips, kind="reg", stat_func=stats.pearsonr)
+# tips = sns.load_dataset("tips")
+# with sns.axes_style("darkgrid"): 
+#     ax = sns.jointplot("total_bill", "tip", data=tips, kind="reg", stat_func=stats.pearsonr)
 
-plt.show()
-sys.exit()
+# plt.show()
+# sys.exit()
 
+
+
+# 将画布设置为A4纸
+# a4_dims = (11.7, 8.27)
+# iris = sns.load_dataset("iris")
+# with sns.axes_style("darkgrid"):
+#     # 设置画布大小
+#     # f, ax = plt.subplots(figsize=a4_dims)
+#     fig = sns.pairplot(df_train[['BsmtFinType2','BsmtUnfSF','BsmtFinSF2']], hue='BsmtFinType2', palette="Set3")
+    
+# plt.show()
+# sys.exit()
 
 # 多变量散点图，也可以理解为 回归图
-with sns.axes_style("darkgrid"):
-    f, ax = plt.subplots(figsize=a4_dims)
-    data = pd.concat([df_train['SalePrice'],df_train["GrLivArea"]],axis = 1)
-    fig = sns.regplot(x="GrLivArea", y="SalePrice", data=data, fit_reg=False) # fit_reg is True, estimate and plot a regression model relating the x and y variables.
-    fig.axis(xmin=0, xmax=6000, ymin=0,ymax=800000)
-    fig.set_title('Plot data and a linear regression model fit.', weight='bold') # 设置标题
-plt.show()
+# with sns.axes_style("darkgrid"):
+#     f, ax = plt.subplots(figsize=a4_dims)
+#     data = pd.concat([df_train['SalePrice'],df_train["GrLivArea"]],axis = 1)
+#     fig = sns.regplot(x="GrLivArea", y="SalePrice", data=data, fit_reg=True) # fit_reg is True, estimate and plot a regression model relating the x and y variables.
+#     fig.axis(xmin=0, xmax=6000, ymin=0,ymax=800000)
+#     fig.set_title('Plot data and a linear regression model fit.', weight='bold') # 设置标题
+# plt.show()
+# sys.exit()
 
-
-sys.exit()
 
 
 # box plot overallqual / saleprice  
-data = pd.concat([df_train['SalePrice'],df_train['OverallQual']],axis = 1)
-with sns.axes_style("darkgrid"):
-    f, ax = plt.subplots(figsize=a4_dims)
-    fig = sns.boxplot(x='OverallQual', y='SalePrice', data=data, ax=ax)
-    fig.axis(ymin=0, ymax=800000)
-    fig.set_title('Draw a box plot to show distributions with respect to categories.', weight='bold').set_fontsize('14') # 给标题设置字体和加粗
-plt.show()
-
+# data = pd.concat([df_train['SalePrice'],df_train['OverallQual']],axis = 1)
+# with sns.axes_style("darkgrid"):
+#     f, ax = plt.subplots(figsize=a4_dims)
+#     fig = sns.boxplot(x='OverallQual', y='SalePrice', data=data, ax=ax, palette="Set3")
+#     # fig.axis(ymin=0, ymax=800000)
+#     fig.set_title('Draw a box plot to show distributions with respect to categories.', weight='bold').set_fontsize('14') # 给标题设置字体和加粗
+# plt.show()
+# sys.exit()
 
 # boxplot saleprice / yearbuilt  
-var = 'YearBuilt'
-data = pd.concat([df_train['SalePrice'],df_train['YearBuilt']],axis = 1)
-with sns.axes_style("darkgrid"):
-    f, ax = plt.subplots(figsize=a4_dims)
-    fig = sns.boxplot(x=var, y='SalePrice', data=data, ax=ax) 
-    fig.axis(ymin=0,ymax=800000)  
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=90) # x轴标签，旋转90度
-    fig.set_title('Draw a box plot to show distributions with respect to categories.', weight='bold') # 给标题设置字体和加粗
-plt.show()
-
+# var = 'YearBuilt'
+# data = pd.concat([df_train['SalePrice'],df_train['YearBuilt']],axis = 1)
+# with sns.axes_style("darkgrid"):
+#     f, ax = plt.subplots(figsize=a4_dims)
+#     fig = sns.boxplot(x=var, y='SalePrice', data=data, ax=ax) 
+#     fig.axis(ymin=0,ymax=800000)  
+#     ax.set_xticklabels(ax.get_xticklabels(), rotation=90) # x轴标签，旋转90度
+#     fig.set_title('Draw a box plot to show distributions with respect to categories.', weight='bold') # 给标题设置字体和加粗
+# plt.show()
+# sys.exit()
 
 # correlation matrix, 相关矩阵  
 corrmat = df_train.corr() 
-f, ax = plt.subplots(figsize=a4_dims) 
-# make limits of the colormap is between -1 and 1 and plot a heatmap for data centered on 0 with a diverging colormap
-fig = sns.heatmap(corrmat, cmap='RdBu', linewidths=0.05, vmin=-1, vmax=1, center=0) 
-fig.set_title('Plot rectangular data as a color-encoded matrix.', weight='bold') # 设置标题
-plt.show()
+# f, ax = plt.subplots(figsize=a4_dims) 
+# # make limits of the colormap is between -1 and 1 and plot a heatmap for data centered on 0 with a diverging colormap
+# fig = sns.heatmap(corrmat, cmap='RdBu', linewidths=0.05, vmin=-1, vmax=1, center=0) 
+# fig.set_title('Plot rectangular data as a color-encoded matrix.', weight='bold') # 设置标题
+# plt.show()
+# sys.exit()
 
 # 取与目标变量相关系数最大的10个和最小的10个变量，绘制重要变量的相关系数热力图
-top_num = 10  
-cols_max = corrmat.nlargest(top_num,'SalePrice')['SalePrice'].index   # 取出与saleprice相关性最大的十项
-cols_min = corrmat.nsmallest(top_num,'SalePrice')['SalePrice'].index  # 取出与saleprice相关性最小的十项
-cols = list(cols_max) + list(cols_min)
-cols = list(set(cols))
-cm = np.corrcoef(df_train[cols].values.T)  #相关系数
-f, ax = plt.subplots(figsize=a4_dims) 
-hm_first = sns.heatmap(cm, linewidths=0.05, cbar=True, vmin=-1, vmax=1, center=0,
-                      annot=True, square=True, fmt='.2f', annot_kws={'size': 8}, yticklabels=cols, xticklabels=cols, cmap='RdBu')
+# top_num = len(df_train)
+# cols_max = corrmat.nlargest(top_num,'SalePrice')['SalePrice'].index   # 取出与saleprice相关性最大的十项
+# cols_min = corrmat.nsmallest(top_num,'SalePrice')['SalePrice'].index  # 取出与saleprice相关性最小的十项
+# cols = list(cols_max) + list(cols_min)
+# cols = list(set(cols))
 
-# 突出一些特殊的数字
-for text in hm_first.texts:
-    value_num = abs(float(text.get_text())) # 取绝对值
-    if value_num >= 0.7 and value_num != 1.00:
-        text.set_fontsize(9)
-        text.set_weight('bold')
-        text.set_color('orangered')
-hm_first.set_title('Plot rectangular data as a color-encoded matrix of important variables', weight='bold') # 设置标题
-plt.show()
+# cols = corrmat.nlargest(top_num, 'SalePrice')['SalePrice'].index
+# cm = np.corrcoef(df_train[cols].values.T)  #相关系数
+# sns.set(font_scale=1.25)
+# hm_first = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, 
+#                          yticklabels=cols.values, xticklabels=cols.values)
+# plt.show()
+# sys.exit()
 
+# with sns.axes_style("darkgrid"):
+#     f, ax = plt.subplots(figsize=a4_dims) 
+#     # hm_first = sns.heatmap(cm, linewidths=0.1, cbar=True, vmin=-1, vmax=1, center=0,
+#     #                    annot=True, square=True, fmt='.2f', annot_kws={'size': 8}, yticklabels=cols, xticklabels=cols, cmap='RdBu')
+#     hm_first = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, 
+#                          yticklabels=cols, xticklabels=cols)
+
+
+
+# # 只突出一些特殊的数字
+# for text in hm_first.texts:
+#     value_num = abs(float(text.get_text())) # 取绝对值
+#     if value_num >= 0.6 and value_num != 1.00:
+#         text.set_fontsize(8)
+#         text.set_weight('bold')
+#         text.set_color('orangered')
+#     else:
+#         text.set_alpha(0)
+# hm_first.set_title('Plot rectangular data as a color-encoded matrix of important variables', weight='bold') # 设置标题
+# plt.show()
+# sys.exit()
 """
 热力图参数说明：
 - annotate的缩写，annot默认为False，当annot为True时，在heatmap中每个方格写入数据，annot_kws，当annot为True时，可设置各个参数，包括大小，颜色，加粗，斜体字等
@@ -176,54 +203,72 @@ plt.show()
 """
 
 # 查看各字段的缺失，以百分比的形式显示
-total = df_train.isnull().sum().sort_values(ascending=False)
-percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending = False)
-missing_data = pd.concat([total,percent], axis=1, keys=['Total','Percent'])
-missing_data.head(20)
-
+# total = df_train.isnull().sum().sort_values(ascending=False)
+# percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending = False)
+# missing_data = pd.concat([total,percent], axis=1, keys=['Total','Percent'])
+# missing_data.head(20)
+# sys.exit()
 
 # 数据标准化的demo，此处以“SalePrice”为例。
-with sns.axes_style("darkgrid"):
-    f, ax = plt.subplots(figsize=a4_dims) # 设置画布大小
-    saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis])
-    low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
-    high_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
-    fig = sns.distplot(saleprice_scaled)
-    fig.set_title('Flexibly plot a univariate distribution of observations.', weight='bold') # 设置标题
-plt.show()
+# with sns.axes_style("darkgrid"):
+#     f, ax = plt.subplots(figsize=a4_dims) # 设置画布大小
+#     saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis])
+#     low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+#     high_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+#     fig = sns.distplot(saleprice_scaled)
+#     fig.set_title('Flexibly plot a univariate distribution of observations.', weight='bold') # 设置标题
+# plt.show()
 
-print('outer range(low) of the distribution :','\n',low_range)
-print ('outer range (high) of the distribution :','\n',high_range)
+# print('outer range(low) of the distribution :','\n',low_range)
+# print ('outer range (high) of the distribution :','\n',high_range)
+# sys.exit()
+
 
 # 双变量数据分析  Bivariate analysis
-var = 'GrLivArea'  
-data = pd.concat([df_train['SalePrice'],df_train[var]], axis=1)
-with sns.axes_style("darkgrid"):
-    # sns.jointplot(x=var, y='SalePrice',data=data,kind='kde')
-    # sns.jointplot(x=var, y='SalePrice',data=data,kind='scatter')
-    fig = sns.jointplot(x=var, y='SalePrice', data=data, kind='reg')  # this kind may be better.
-    # sns.jointplot(x=var, y='SalePrice',data=data,kind='resid')
-    # sns.jointplot(x=var, y='SalePrice',data=data,kind='hex')
-plt.show()
+# var = 'GrLivArea'  
+# data = pd.concat([df_train['SalePrice'],df_train[var]], axis=1)
+# with sns.axes_style("darkgrid"):
+#     # sns.jointplot(x=var, y='SalePrice',data=data,kind='kde')
+#     # sns.jointplot(x=var, y='SalePrice',data=data,kind='scatter')
+#     fig = sns.jointplot(x=var, y='SalePrice', data=data, kind='reg')  # this kind may be better.
+#     # sns.jointplot(x=var, y='SalePrice',data=data,kind='resid')
+#     # sns.jointplot(x=var, y='SalePrice',data=data,kind='hex')
+# plt.show()
 
 
 # 正态性检验
 # in the search for normality  
 # histogram and normal probability plot  直方图和正态概率图
 # 设置画布_1的size
-with sns.axes_style("darkgrid"):
-    f, ax= plt.subplots(figsize=a4_dims)
-    fig = sns.distplot(df_train['SalePrice'], fit = norm, ax=ax) # fit 控制拟合的参数分布图形
-    fig.set_title('Flexibly plot a univariate distribution of observations.', weight='bold') # 设置标题
-plt.show()
+# with sns.axes_style("darkgrid"):
+#     f, ax= plt.subplots(figsize=a4_dims)
+#     fig = sns.distplot(df_train['SalePrice'], fit = norm, ax=ax) # fit 控制拟合的参数分布图形
+#     fig.set_title('Flexibly plot a univariate distribution of observations.', weight='bold') # 设置标题
+# plt.show()
+# sys.exit()
 
-with sns.axes_style("darkgrid"):
-    # 设置画布_2的size
-    f, ax= plt.subplots(figsize=a4_dims)
-    # probplot :Calculate quantiles for a probability plot, and optionally show the plot. 计算概率图的分位数
-    res = stats.probplot(df_train['SalePrice'], plot=plt) # seaborn画不了Q-Q图~
-plt.show()
 
+# with sns.axes_style("darkgrid"):
+#     # 设置画布_2的size
+#     f, ax= plt.subplots(figsize=a4_dims)
+#     # probplot :Calculate quantiles for a probability plot, and optionally show the plot. 计算概率图的分位数
+#     res = stats.probplot(df_train['SalePrice'], plot=plt) # seaborn画不了Q-Q图~
+# plt.show()
+# sys.exit()
+
+
+all_df = pd.concat((df_train.loc[:,'MSSubClass':'SaleCondition'], df_test.loc[:,'MSSubClass':'SaleCondition']), axis=0, ignore_index=True)
+all_df['MSSubClass'] = all_df['MSSubClass'].astype(str)
+quantitative = [f for f in all_df.columns if all_df.dtypes[f] != 'object']
+qualitative = [f for f in all_df.columns if all_df.dtypes[f] == 'object']
+print("quantitative: {}, qualitative: {}" .format (len(quantitative),len(qualitative)))
+
+f = pd.melt(all_df, value_vars=quantitative[0:8])
+with sns.axes_style("darkgrid"):
+    g = sns.FacetGrid(f, col="variable", col_wrap=4, sharex=False, sharey=False)
+    g = g.map(sns.distplot, "value")
+plt.show()
+sys.exit()
 
 # 一次性画多个图的demo
 # 将画布设置为一列两行
